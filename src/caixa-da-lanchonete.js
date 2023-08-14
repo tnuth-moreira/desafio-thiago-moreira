ddd1f80a64445d80bc9b06544b0196cdb8592ca6
tnuth-moreira <tnuth.moreira@gmail.com>
2023-08-14 15:55:40 -0300

    iniciando o desafio

---
+++ src/caixa-da-lanchonete.js
@@ -0,0 +1,29 @@
+const formaDePagamento = [debito, credito, dinheiro];
+
+
+
+class CaixaDaLanchonete {
+
+    calcularValorDaCompra(metodoDePagamento, itens) {
+        let cardapio = {
+            cafe: 3,
+            chantily: 1.5,
+            suco: 6.2,
+            sanduiche: 6.5,
+            queijo: 2,
+            salgado: 2,
+            combo1: 9.5,
+            combo2: 7.5
+        };
+
+        let itensComprados = [];
+
+        let total = 0;
+
+
+        return "";
+    }
+
+}
+
+export { CaixaDaLanchonete };
